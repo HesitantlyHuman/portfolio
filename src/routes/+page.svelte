07965e01182b2c84b4e4727797067d1046504c25
GenerallyIntelligent <tannerjeffreysims@gmail.com>
2023-05-11 17:11:17 -0600

    Switch to self-hosted fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,31 +40,7 @@
     }
 </script>
 
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-
-<link
-    rel="preload"
-    as="style"
-    href="https://fonts.googleapis.com/css?family=Work+Sans:400,600,700&display=swap"
-/>
-<link
-    rel="preload"
-    as="style"
-    href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300,700&display=swap"
-/>
-
-<link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Work+Sans:400,600,700&display=swap"
-    media="print"
-    onload="this.media='all'"
-/>
-<link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300,700&display=swap"
-    media="print"
-    onload="this.media='all'"
-/>
+<link href="/styles/fonts.css" rel="stylesheet" />
 
 <Navigation theme_function={toggleTheme} theme={theme.name} height="5em" />
 <ContentColumn>
